113f2ba1e59102321b1baa0e5856074212d62831
liyi <991230629@qq.com>
2023-05-13 12:22:47 +0800

    commit

--- src/store/login.Store.js
+++ src/store/login.Store.js
@@ -1,8 +1,8 @@
 /*
  * @Author: liyi
- * @LastEditors: liyi 991230629@qq.com
+ * @LastEditors: liyi
  * @Date: 2023-04-29 22:32:06
- * @LastEditTime: 2023-05-13 10:36:53
+ * @LastEditTime: 2023-05-13 12:22:26
  */
 import { makeAutoObservable } from 'mobx'
 import { http } from '@/utils'
@@ -12,17 +12,11 @@ class LoginStore {
     makeAutoObservable(this)
   }
   login = async ({ name, password }) => {
-    const res = await http.post('/user/login',{name,password})
-    if(res.data.code==="0"&&res.data.data){
-        localStorage.setItem("userInfo",JSON.stringify(res.data.data))
+    const res = await http.post('/user/login', { name, password })
+    if (res.data.code === '0' && res.data.data) {
+      localStorage.setItem('userInfo', JSON.stringify(res.data.data))
     }
   }
-  // http://112.124.27.151:8081/user/register
-  // register=async ({ name, password }) => {
-  //   const res = await http.post(`/user/register?name=${name}&password=${password}`)
-  //   this.token = res.data
-  //   console.log(res);
-  // }
 }
 
 export default LoginStore
